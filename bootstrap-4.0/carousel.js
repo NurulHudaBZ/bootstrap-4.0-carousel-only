c9792f0ac550ab18a1fddda18b622d2f0b9dcb3a
mekkli <gui.klimek22@gmail.com>
2018-01-31 17:23:58 +0100

    remove forgot Bootstrap external elements

--- bootstrap-4.0/carousel.js
+++ bootstrap-4.0/carousel.js
@@ -716,17 +716,9 @@ var Carousel = function ($$$1) {
 })($);
 
 exports.Util = Util;
-exports.Alert = Alert;
-exports.Button = Button;
 exports.Carousel = Carousel;
-exports.Collapse = Collapse;
-exports.Dropdown = Dropdown;
-exports.Modal = Modal;
-exports.Popover = Popover;
-exports.Scrollspy = ScrollSpy;
-exports.Tab = Tab;
-exports.Tooltip = Tooltip;
 
 Object.defineProperty(exports, '__esModule', { value: true });
 
 })));
+
